f1cc48db26fdb9abc004e7395326b3223d3b4903
ElisangelaBatestucci <zhanbatestucci@hotmail.com>
2021-11-30 14:56:51 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,20 @@
+﻿// Primeiro Programa no IDE
+
+using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Hello, ";
+            var b = "World!";
+            Console.WriteLine( a + b);
+
+        }
+    }
+}
+
+
+
